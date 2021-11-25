994b2b23e91e7d4e3e9fa58585ea8005c75d81a3
Evan Nemetz <evannemetz@gmail.com>
2021-11-24 21:02:31 -0500

    Initial Commit

---
+++ Budge/BudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgeApp.swift
+//  Budge
+//
+//  Created by Evan Nemetz on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct BudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
